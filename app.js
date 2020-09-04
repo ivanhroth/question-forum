ea09244754622020563b86fd27072d1758426d0f
Ivan Roth <ivan.h.roth@gmail.com>
2020-09-04 13:05:45 -0700

    add boilerplate routing and express

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const morgan = require("morgan");
+const usersRouter = require("./routes/users");
+const questionsRouter = require("./routes/questions");
 
+const app = express();
+
+app.use("/users", usersRouter);
+app.use("/questions", questionsRouter);
