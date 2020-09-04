1f858ede465757d83117b0fcf5278574dace7fed
Ivan Roth <ivan.h.roth@gmail.com>
2020-09-04 15:37:24 -0700

    add utils.js with asyncHandler and handleValidationErrors

---
+++ utils.js
@@ -0,0 +1,19 @@
+const { validationResult } = require('express-validator');
+
+const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch();
+
+const handleValidationErrors = (req, res, next) => {
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+        const errors = validationErrors.array().map((err)=> err.msg);
+        const error = Error('bad request');
+
+        error.errors = errors;
+        error.status = 400;
+        error.title = 'Bad Request';
+        return next(error);
+    }
+    next();
+}
+
+module.exports = { asyncHandler, handleValidationErrors };
