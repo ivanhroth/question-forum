e0f3d2f28107a5be5074e5433edf4e5f48586dab
alec <alecbgarcia@gmail.com>
2020-09-06 01:26:02 -0700

    finished user login and sign up

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ const express = require("express");
 const morgan = require("morgan");
 const usersRouter = require("./routes/users");
 const questionsRouter = require("./routes/questions");
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const { environment, sessionSecret } = require('./config');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
+app.use(morgan('dev'));
+app.use(cookieParser(sessionSecret));
+
+app.use(session({
+  name: 'question-forum.sid',
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/users", usersRouter);
 app.use("/questions", questionsRouter);
