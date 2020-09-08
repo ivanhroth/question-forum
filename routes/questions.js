21669bd0bf0579fb128bcb2f1a9477a95cd21973
Ivan Roth <ivan.h.roth@gmail.com>
2020-09-07 18:00:31 -0700

    add basic question viewing functionality

--- routes/questions.js
+++ routes/questions.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const db = require('../models');
+const { Question, User, Answer } = db;
+const { asyncHandler } = require('./utils');
 
 const router = express.Router();
+
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const question = await Question.findByPk(id);
+    const user = await User.findByPk(question.userId);
+    const answers = await Answer.findAll({
+        where: {
+            questionId: id
+        }
+    });
+    res.render('question', { title: `Question: ${question.title}`, question, user, answer, csrfToken: req.csrfToken() })
+}))
 
 module.exports = router;
