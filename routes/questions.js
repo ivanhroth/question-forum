8c2f440bd450ef6f13514fe3b5f8ec30c4633d3d
Ivan Roth <ivan.h.roth@gmail.com>
2020-09-07 18:19:19 -0700

    fix minor issues and add an (incomplete) answer form

--- routes/questions.js
+++ routes/questions.js
@@ -9,12 +9,18 @@ router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
     const id = req.params.id;
     const question = await Question.findByPk(id);
     const user = await User.findByPk(question.userId);
+    const answerSubmitURL = `/questions/${id}/answer`;
     const answers = await Answer.findAll({
         where: {
             questionId: id
         }
     });
-    res.render('question', { title: `Question: ${question.title}`, question, user, answers })
+    res.render('question', { title: `Question: ${question.title}`, question, user, answers, answerSubmitURL })
+}));
+
+router.post('/:id(\\d+)', asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const { answerMessage } = req.body;
 }))
 
 module.exports = router;
