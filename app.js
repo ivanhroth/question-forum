40e7b26661ddd426a03fe2d5187668b5571ffc3f
alec <alecbgarcia@gmail.com>
2020-09-09 10:49:56 -0700

    fixed logout to delete session

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(cookieParser(sessionSecret));
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.render('layout')
 });
 
 app.use(session({
