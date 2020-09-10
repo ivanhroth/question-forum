170bf48866dc352b881ecb9fba7380848ffe0e93
Ivan Roth <ivan.h.roth@gmail.com>
2020-09-10 09:07:50 -0700

    fix misleading comment

--- public/js/question-view.js
+++ public/js/question-view.js
@@ -19,9 +19,8 @@ document.addEventListener("DOMContentLoaded", async () => {
             console.log(e);
         }
         console.log("Got here!");
-        const homeURL = ['http:/', window.location.href.split('/')[2], 'questions'].join('/');
+        const homeURL = ['http:/', window.location.href.split('/')[2], 'questions'].join('/'); // redirects to the questions list without referencing "localhost" by modifying the current URL
         console.log(homeURL);
         window.location.href = homeURL;
-        // http: - localhost:8080
     })
 });
